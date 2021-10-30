1077906d4b8bafcb975d2db9cf619bc88856fb1e
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-10-30 23:24:02 +0700

    jwt review

--- src/middleware/jwt.js
+++ src/middleware/jwt.js
@@ -3,3 +3,5 @@ const jwt = require("jsonwebtoken");
 const createJWT = (user) => {
     const token = jwt.sign({user: user.dataValues}, "private-key-classroom", )
 }
+
+//belom kelar, butuh nonton record pas review dlu baru dikelarin
